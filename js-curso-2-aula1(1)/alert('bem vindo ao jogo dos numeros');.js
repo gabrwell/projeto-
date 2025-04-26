e4f8eb89a9b9490716f58f39ed1e83e274faf3b7
gabrwell <gabrielgibin0504@gmail.com>
2025-04-26 13:58:31 -0300

    Delete js-curso-2-aula1(1) directory

--- js-curso-2-aula1(1)/alert('bem vindo ao jogo dos numeros');.js
+++
@@ -1,9 +0,0 @@
-alert('bem vindo ao jogo dos numeros');
-
-let pontuacao = prompt('Escolha um numero');
-
-if(pontuacao >= 100){
-    alert('Parabéns voce venceu');
-} else{
-    alert('Voce errou! Tente novamente');
-}
